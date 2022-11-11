0f242cc402ba9e57beaaad0fa63ef4aef713ad46
Santeri Simanainen <santeri.simanainen97@gmail.com>
2022-11-11 11:45:52 +0530

    Added explicit loss method

--- recommender.py
+++ recommender.py
@@ -23,8 +23,8 @@ class FunkSVD:
         
         # Vectorize dataframe for faster iteration and computation 
         self._ratings = ratings["rating"].to_numpy()
-        self._users = ratings["userId"].to_numpy(dtype = int)
-        self._items = ratings["itemId"].to_numpy(dtype = int)
+        self._users = ratings["userId"].to_numpy(dtype = np.int32)
+        self._items = ratings["itemId"].to_numpy(dtype = np.int32)
         
         # Global average that normalizes the data
         # Note: no explicit normalization is needed because the model takes care of it
@@ -32,26 +32,29 @@ class FunkSVD:
         
         # Initialize parameters somehow
         
-        # TODO: these cannot be zeros
-        self._P = np.random.rand(self._n_latent_factors,  self._n_users)-0.5
-        self._Q = np.random.rand(self._n_latent_factors,  self._n_items)-0.5
+        # This is pretty arbitrary but seems to work well
+        self._P = (np.random.rand(n_latent_factors,  n_users)-0.5)/np.sqrt(n_latent_factors)
+        self._Q = (np.random.rand(n_latent_factors,  n_items)-0.5)/np.sqrt(n_latent_factors)
         
         # These can be either zeros or something else
-        self._user_bias = np.zeros(shape = [self._n_users,])
-        self._item_bias = np.zeros(shape = [self._n_items,])
+        self._user_bias = np.zeros(shape = [n_users,])
+        self._item_bias = np.zeros(shape = [n_items,])
         
         
     def train(self, learning_rate = 0.005, reg_coefficient = 0.02, tolerance = 1e-5, max_epochs = 100):
+        '''
+        TODO. Trains the model using stochastic gradient descent.
         
         '''
-        Trains the model using stochastic gradient descent.
-        TODO, CURRENTLY TRAINS ONLY FOR ONE EPOCH
-        
-        '''
-        self._train_epoch(learning_rate, reg_coefficient)
-        
-        # Give some user feedback 
-        print(self.L2_error())
+        # Testing whether all works as expected
+        for epochs in range(max_epochs):
+            
+            self._train_epoch(learning_rate, reg_coefficient)
+            loss = self._loss(reg_coefficient)
+            err = self.L2_error()
+            print("Loss: {0:.3f}, error: {1:.3f}".format(loss, err))
+            
+            
         return None
           
     def predict(self, userId, itemId):
@@ -69,8 +72,9 @@ class FunkSVD:
                  If inputs given as np.arrays of same shape, returns an np.array with rating for each pair
                  If inputs given as pd.Series returns a pd.DataFrame in similar form as input data
         '''
-        # Input type <int>
-        if isinstance(userId, int) and isinstance(itemId, int):
+        # Input type <int> or <np.int32>
+        if (isinstance(userId, int) and isinstance(itemId, int)) or \
+            (isinstance(userId, np.int32) and isinstance(itemId, np.int32)):           
             p = self.user_latent_factors(userId)
             q = self.item_latent_factors(itemId)
             user_bias = self.user_bias(userId)
@@ -81,8 +85,8 @@ class FunkSVD:
         else:
             # Determine type and copy to numpy variables
             if isinstance(userId, pd.Series) and isinstance(userId, pd.Series):
-                users = userId.toNumpy(dtype = int)
-                items = itemId.toNumpy(dtype = int)             
+                users = userId.toNumpy(dtype = np.int32)
+                items = itemId.toNumpy(dtype = np.int32)             
             elif isinstance(userId, np.ndarray) and isinstance(userId, np.ndarray):
                 users = userId
                 items = itemId
@@ -92,10 +96,11 @@ class FunkSVD:
             # Check that the arrays are of the same size
             if userId.shape != itemId.shape:
                 raise Exception("User and item arrays should be the same size")    
+            
             ratings_tmp = np.zeros(shape = userId.shape)
             
             # Compute the predictions
-            for index in range(len(ratings)):
+            for index in range(len(ratings_tmp)):
                 p = self.user_latent_factors(users[index])
                 q = self.item_latent_factors(items[index])
                 user_bias = self.user_bias(users[index])
@@ -183,8 +188,8 @@ class FunkSVD:
         
         for index in indices:
 
-            userId = int(self._users[index])
-            itemId = int(self._items[index])
+            userId = self._users[index]
+            itemId = self._items[index]
             rating = self._ratings[index]
             
             # Current parameters corresponding to the training instance
@@ -196,7 +201,7 @@ class FunkSVD:
             # Difference between predicted and real ratings
             dif = rating - self.predict(userId, itemId)
             
-            # Update parameters by Funk SGD rule
+            # Update parameters:
             self._user_bias[userId-1] = user_bias + learning_rate*(dif - reg_coefficient*user_bias)
             self._item_bias[itemId-1] = item_bias + learning_rate*(dif - reg_coefficient*item_bias)
             self._P[:,userId-1] = p + learning_rate*(dif*q - reg_coefficient*p)
@@ -204,4 +209,13 @@ class FunkSVD:
         
         return None
     
+    def _loss(self, reg_coefficient):
+        err = self.L2_error()**2
+        reg_bias = np.sum(self._user_bias**2) + np.sum(self._item_bias**2)
+        reg_latent_factors = np.sum(self._P**2) + np.sum(self._Q**2)
+        loss = err + reg_coefficient*(reg_bias +  reg_latent_factors)
+        return loss
+            
+        
+        
     # ... other auxiliary private methods.
